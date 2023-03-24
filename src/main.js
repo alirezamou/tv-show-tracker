677c864cbd078cac2da6aca3274479ea992edb51
alirezamou <am808036@gmail.com>
2023-03-24 11:33:01 +0330

    install font-awesome

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import router from "./router";
 
 import "bulma/css/bulma.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret);
+
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(router);
 
