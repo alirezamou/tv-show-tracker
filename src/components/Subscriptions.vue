f73718221ce3fb551897ab67b08cb780914bd2d7
alirezamou <am808036@gmail.com>
2023-03-24 16:28:35 +0330

    add 'Subscriptions.vue' comp and add it's route to root path

---
+++ src/components/Subscriptions.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ShowSearch />
+    </div>
+</template>
+
+<script>
+import ShowSearch from './ShowSearch.vue';
+
+export default {
+    name: "Subscriptions",
+    components: {
+        ShowSearch,
+    }
+}
+</script>
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Subscriptions from "@/components/Subscriptions.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: Subscriptions,
+    },
+  ],
 });
 
 export default router;
